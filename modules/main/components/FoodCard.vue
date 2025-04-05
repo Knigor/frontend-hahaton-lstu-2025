6910a53f7c44c144cb04dfa56e1468c92fd0e8c1
maxwaraxe <makskriper9@gmail.com>
2025-04-05 17:58:47 +0300

    eggs

--- modules/main/components/FoodCard.vue
+++ modules/main/components/FoodCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grad flex h-[125px] w-[220px] flex-col items-start rounded-2xl border-[1px] border-solid border-[#D1D1D1] p-4"
+    class="grad flex h-[125px] min-w-[220px] flex-col items-start rounded-2xl border-[1px] border-solid border-[#D1D1D1] p-4"
   >
     <div class="text-[20px] font-medium">{{ props.name }}</div>
     <div class="text-[16px] font-medium">{{ props.value }}</div>
@@ -16,11 +16,6 @@ const props = defineProps({
 
 <style scoped>
 .grad {
-  background: rgb(66, 42, 213);
-  background: linear-gradient(
-    165deg,
-    rgba(66, 42, 213, 0.6208684157256652) 15%,
-    rgba(7, 99, 23, 0.3435574913559174) 87%
-  );
+  background-color: rgba(0, 255, 255, 0.116);
 }
 </style>
