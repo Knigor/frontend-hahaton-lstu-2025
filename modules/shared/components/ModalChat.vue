b49562eb2fedc8cb6ab89db8f183f1e452e2e151
MaxWarAxe <113930738+MaxWarAxe@users.noreply.github.com>
2025-04-06 06:25:18 +0300

    fix

--- modules/shared/components/ModalChat.vue
+++ modules/shared/components/ModalChat.vue
@@ -1,84 +1,44 @@
 <template>
   <TransitionRoot appear :show="isOpen" as="template">
     <Dialog as="div" @close="closeModal" class="relative z-10">
-      <TransitionChild
-        as="template"
-        enter="duration-300 ease-out"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="duration-200 ease-in"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+        leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-black/25" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-y-auto">
-        <div
-          class="flex min-h-full items-center justify-center p-4 text-center"
-        >
-          <TransitionChild
-            as="template"
-            enter="duration-300 ease-out"
-            enter-from="opacity-0 scale-95"
-            enter-to="opacity-100 scale-100"
-            leave="duration-200 ease-in"
-            leave-from="opacity-100 scale-100"
-            leave-to="opacity-0 scale-95"
-          >
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95">
             <DialogPanel
-              class="max-w-[650px]transform relative h-[800px] w-[650px] overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
-            >
+              class="max-w-[650px]transform relative h-[800px] w-[650px] overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
               <div class="flex items-center justify-between">
-                <DialogTitle
-                  as="h3"
-                  class="text-center text-lg leading-6 font-medium text-gray-900"
-                >
+                <DialogTitle as="h3" class="text-center text-lg leading-6 font-medium text-gray-900">
                   Фитнесс-ассистент
                 </DialogTitle>
-                <X
-                  class="btn-circle cursor-pointer p-3 hover:bg-gray-200"
-                  size="32"
-                  @click="closeModal"
-                />
+                <X class="btn-circle cursor-pointer p-3 hover:bg-gray-200" size="32" @click="closeModal" />
               </div>
               <!-- Юзер пишет сообщение -->
-              <div
-                ref="chatContainer"
-                class="flex max-h-[600px] flex-col-reverse gap-4 overflow-y-auto pr-2"
-              >
-                <div
-                  v-for="(msg, index) in [...chat].reverse()"
-                  :key="index"
-                  role="alert"
-                  :class="[
-                    'alert',
-                    msg.from === 'user'
-                      ? 'bg-[#422AD5] text-[#DBD6FC]'
-                      : 'bg-gray-200'
-                  ]"
-                >
+              <div ref="chatContainer" class="flex max-h-[600px] flex-col-reverse gap-4 overflow-y-auto pr-2">
+                <div v-for="(msg, index) in [...chat].reverse()" :key="index" role="alert" :class="[
+                  'alert',
+                  msg.from === 'user'
+                    ? 'bg-[#422AD5] text-[#DBD6FC]'
+                    : 'bg-gray-200'
+                ]">
                   <span>{{ msg.text }}</span>
                 </div>
               </div>
 
               <div class="absolute bottom-4 flex gap-2">
-                <textarea
-                  v-model="message"
-                  class="textarea w-[500px] resize-none border border-[#422AD5]"
-                  placeholder="Введите сообщение..."
-                  @keydown.enter.prevent="sendMessage"
-                ></textarea>
+                <textarea v-model="message" class="textarea w-[500px] resize-none border border-[#422AD5]"
+                  placeholder="Введите сообщение..." @keydown.enter.prevent="sendMessage"></textarea>
 
                 <div
                   class="mb-auto flex cursor-pointer flex-row rounded-2xl bg-[#422AD5] p-2 transition duration-300 hover:rounded-2xl hover:bg-blue-600"
-                  @click="sendMessage"
-                >
-                  <SendHorizontal
-                    stroke-width="1.5"
-                    color="#fff"
-                    class="h-8 w-8"
-                  />
+                  @click="sendMessage">
+                  <SendHorizontal stroke-width="1.5" color="#fff" class="h-8 w-8" />
                 </div>
               </div>
             </DialogPanel>
@@ -92,6 +52,9 @@
 <script setup>
 import { ref } from 'vue'
 import { X, SendHorizontal } from 'lucide-vue-next'
+import { useChat } from '../composables/useChat'
+const { UseSendMessage } = useChat()
+
 import {
   TransitionRoot,
   TransitionChild,
@@ -132,6 +95,7 @@ function sendMessage() {
 input {
   width: 100%;
 }
+
 select {
   width: 100%;
 }
--- modules/shared/composables/useChat.ts
+++ modules/shared/composables/useChat.ts
@@ -1,17 +1,20 @@
 import type { $Fetch } from 'ofetch'
 
-export const usePlan = () => {
+export const useChat = () => {
   const { $protectedApi } = useNuxtApp() as unknown as { $protectedApi: $Fetch }
 
-  async function getPlan() {
+  async function UseSendMessage(message: string) : Promise<{ response?: any; success: boolean; error?: any }> {
     try {
-      const result = await $protectedApi('fitness/plan', {
-        method: 'GET',
+      const result = await $protectedApi('fitness/chat/send', {
+        method: 'POST',
+        body:{
+          "text": message
+        }
       })
       return { response : result, success: true }
     } catch (error) {
       return { success: false, error }
     }
   }
-  return { getPlan }
+  return { UseSendMessage }
 }
