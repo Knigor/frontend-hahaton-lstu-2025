b8fc2744747c62550995b67ada2e3a005c803c0f
MaxWarAxe <113930738+MaxWarAxe@users.noreply.github.com>
2025-04-06 05:43:05 +0300

    plan done

--- modules/main/components/FoodCard.vue
+++ modules/main/components/FoodCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="grad flex h-[125px] min-w-[220px] flex-col items-start rounded-2xl border-[1px] border-solid border-[#D1D1D1] p-4"
-  >
-    <div class="text-[20px] font-medium">{{ props.name }}</div>
-    <div class="text-[16px] font-medium">{{ props.value }}грамм.</div>
+    class="grad flex h-[125px] min-w-[220px] flex-col items-start rounded-2xl border-[1px] border-solid border-[#D1D1D1] p-4">
+    <div class="text-[16px] text-start font-semibold text-wrap">{{ props.name }}</div>
+    <div class="text-[16px] text-gray-500 font-medium flex flex-row">{{ props.value }} грамм.
+    </div>
   </div>
 </template>
 
